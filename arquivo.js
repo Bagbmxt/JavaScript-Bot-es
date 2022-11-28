e1064ad0a2e476d50cc26c9765f72ff873947366
SEU <bruno.bmxdabiciceta@gmail.com>
2022-11-28 12:43:00 -0300

    first commit

---
+++ arquivo.js
@@ -0,0 +1,86 @@
+//Função Muda a Cor da Fonte para Amarelo
+function tituloamarelo()
+{ var no = document.getElementById("tituloamarelo")
+no.style.color = 'yellow'; }
+
+function tituloazul()
+{ var no = document.getElementById("tituloazul")
+no.style.color = 'blue'; }
+
+function titulopink()
+{ var no = document.getElementById("titulopink")
+no.style.color = 'pink'; }
+
+function titulopurple()
+{ var no = document.getElementById("titulopurple")
+no.style.color = 'purple'; }
+
+//Função Muda a Cor do Fundo para Verde
+function fundoverde()
+{ var no = document.getElementById("site"); 
+no.style.backgroundColor = '#00FF00'; }
+////////////////////////////////////////
+
+//Função Muda a Cor do Fundo para Azul
+function fundoazul()
+{ var no = document.getElementById("site"); 
+no.style.backgroundColor = '#0461f7'; }
+////////////////////////////////////////
+
+//Função Muda a Cor do Fundo para Azul
+function fundovermelho()
+{ var no = document.getElementById("site"); 
+no.style.backgroundColor = '#f70404'; }
+////////////////////////////////////////
+
+//Função Muda a Cor do Fundo para Preto
+function fundopreto()
+{ var no = document.getElementById("site"); 
+no.style.backgroundColor = '#000000'; }
+////////////////////////////////////////
+
+//Função Muda a Cor do Fundo para Azul
+function fundolaranja()
+{ var no = document.getElementById("site"); 
+no.style.backgroundColor = '#f74100'; }
+////////////////////////////////////////
+
+
+//Função Muda a Fonte para tahoma
+function trocafontetahoma()
+{ var no = document.getElementById("trocafontetahoma"); 
+no.style.fontFamily = 'tahoma'}
+////////////////////////////////////////
+
+//Função Muda a Fonte para ARIAL
+function trocafontearial()
+{ var no = document.getElementById("trocaf"); 
+no.style.fontFamily = 'Arial'}
+////////////////////////////////////////
+
+//Função Muda a Fonte para tahoma
+function trocafonte()
+{ var no = document.getElementById("troc"); 
+no.style.fontFamily = 'Verdana'}
+////////////////////////////////////////
+
+//Função Muda a Fonte para tahoma
+function trocafonttime()
+{ var no = document.getElementById("trocafonti"); 
+no.style.fontFamily = 'Calibri'}
+////////////////////////////////////////
+
+
+
+
+ 
+
+
+
+
+
+
+
+// ID pega o elemento by ID
+// SE O onclick For igual vai pegar o mesmo conteúdo
+// ONCLIC TEM QUE SER O MESMO DA FUNÇÂO
